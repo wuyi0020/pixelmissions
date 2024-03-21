bada249660afb48c3dcf974617ed8bb4ca72f4a6
wuyi0020 <wuyi0020@gmail.com>
2024-03-21 11:30:08 +0800

    Updates

--- assets/ErrorView-CUNbmHQu.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c as r}from"./index-CALbCaTn.js";const t={};function c(n,s,a,_,p,d){return e(),r("div",null,"404 Not Found")}const i=o(t,[["render",c]]);export{i as default};
--- assets/PayDoneView-Ch0fGrg1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as s,b as t}from"./index-CALbCaTn.js";const c={},n=t("div",{class:"container"},"付款完成",-1),a=[n];function r(_,i,d,p,l,f){return o(),s("div",null,a)}const m=e(c,[["render",r]]);export{m as default};
