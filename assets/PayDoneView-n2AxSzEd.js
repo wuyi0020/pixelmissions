84d36ef4901b1578a784b05130a7f9c1f5526a81
wuyi0020 <wuyi0020@gmail.com>
2024-05-23 17:46:40 +0800

    Updates

---
+++ assets/PayDoneView-n2AxSzEd.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,b as t}from"./index-C-_izT2N.js";const c={},n=t("div",{class:"container"},"付款完成",-1),a=[n];function r(_,i,d,p,l,f){return o(),s("div",null,a)}const m=e(c,[["render",r]]);export{m as default};
